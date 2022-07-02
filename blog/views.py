0211e1676cc9923b1045e2f9633c7a8cba71e208
Osama Makhlouph <30751973+Osama-Makhlouph@users.noreply.github.com>
2022-07-02 11:30:13 +0200

    Added comment model, its form, and updated detail view and template to contain comment system!

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,8 @@
-from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 # Create your views here.
 
@@ -34,7 +33,19 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, publish__year=year,
                              publish__month=month, publish__day=day, slug=post, status='published')
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid:
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'form': comment_form, 'comments': comments, 'comment': new_comment})
 
 
 def post_share(request, post_id):
